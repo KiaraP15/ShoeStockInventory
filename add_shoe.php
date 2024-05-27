83dddaa90499e0944e3a5bb0413f801e5daed685
Lance Gasapo <lancegwapogasapo@gmail.com>
2024-05-27 09:46:32 +0800

    this is the updated code

--- add_shoe.php
+++ add_shoe.php
@@ -1,19 +1,6 @@
 <?php
-// Database connection
-$host = 'localhost';
-$dbname = 'shoestock';
-$username = 'InfoSystem';
-$password = 'csab';
+include 'db.php';
 
-try {
-    $conn = new PDO("mysql:host=$host;dbname=shoestock", $username, $password);
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-}
-
-
-// CREATE (Insert)
 function addShoe($brand, $size, $color) {
     global $conn;
     $stmt = $conn->prepare("INSERT INTO Shoes (Brand, Size, Color) VALUES (?, ?, ?)");
@@ -21,7 +8,6 @@ function addShoe($brand, $size, $color) {
     return $conn->lastInsertId();
 }
 
-// READ (Select)
 function getShoes() {
     global $conn;
     $stmt = $conn->prepare("SELECT * FROM Shoes");
@@ -29,7 +15,6 @@ function getShoes() {
     return $stmt->fetchAll(PDO::FETCH_ASSOC);
 }
 
-// READ (Select) a shoe by its ID
 function getShoeById($shoe_id) {
     global $conn;
     $stmt = $conn->prepare("SELECT * FROM Shoes WHERE ShoeID = ?");
@@ -37,15 +22,12 @@ function getShoeById($shoe_id) {
     return $stmt->fetch(PDO::FETCH_ASSOC);
 }
 
-
-// UPDATE
 function updateShoe($shoe_id, $brand, $size, $color) {
     global $conn;
     $stmt = $conn->prepare("UPDATE Shoes SET Brand=?, Size=?, Color=? WHERE ShoeID=?");
     $stmt->execute([$brand, $size, $color, $shoe_id]);
 }
 
-// DELETE
 function deleteShoe($shoe_id) {
     global $conn;
     $stmt = $conn->prepare("DELETE FROM Shoes WHERE ShoeID=?");
